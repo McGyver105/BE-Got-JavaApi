c2a64e2e692d13c15922c0b47b49dd8b495c86d2
Steve <steve.titchener@hotmail.com>
2022-12-29 16:46:34 +0000

    first commit

---
+++ src/main/java/com/be_got_java_api/char_database/CharDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.be_got_java_api.char_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be_got_java_api/char_database/CharDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be_got_java_api.char_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
