0a4c24488efd594b8d2e5a8b31fe077d55af35a3
Steve <steve.titchener@hotmail.com>
2023-01-22 20:22:43 +0000

    first attempt at a query method in the person repository

--- src/main/java/com/be_got_java_api/char_database/repository/PersonRespository.java
+++ src/main/java/com/be_got_java_api/char_database/repository/PersonRespository.java
@@ -1,10 +1,17 @@
 package com.be_got_java_api.char_database.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.be_got_java_api.char_database.model.Person;
 
 public interface PersonRespository extends JpaRepository<Person, Long> {
     
     Boolean existsByPersonName(String personName);
+
+    @Query( "SELECT p FROM Person p WHERE dead = :dead AND religion = :religion" )
+    List<Person> findByDeadAndReligion(@Param("dead") boolean dead, @Param("religion") String religion );
 }
